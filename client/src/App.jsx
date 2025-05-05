1a94810ec5d44daf649cb355f9a85d368eb046b3
BARATH G <chidambaramb2@gmail.com>
2025-05-05 14:37:50 +0530

    add css to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'; // Import the Home component
 import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> {/* Use the imported Home component */}
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
   );
